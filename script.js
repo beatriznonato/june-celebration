da550886b164e3949012c9d6c5762adb77416dca
Beatriz <bia_nonato@icloud.com>
2018-09-25 12:06:43 -0300

    Formar casais que não podem se repetir

---
+++ script.js
@@ -0,0 +1,16 @@
+let meninos = ["Bento", "Zé", "Chico", "Jão"];
+let meninas = ["Maria", "Josefa", "Florentina", "Francisca"];
+
+let i = 0;
+
+while(i < meninos.length){ 
+    let j = 0;
+    
+    while(j < meninas.lenght){
+        
+        console.log(`Casal: ${meninos[i]} e ${meninas(i)}`); //[posição dentro lista]
+        j++;
+    }
+    
+    i++;
+}
