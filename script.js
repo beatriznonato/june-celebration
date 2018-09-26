e593935477c5e6ea3d424534f5c0d1628e33c6e4
Beatriz <bia_nonato@icloud.com>
2018-09-25 22:44:43 -0300

    corrigir sintaxe

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ let i = 0;
 while(i < meninos.length){ 
     let j = 0;
     
-    while(j < meninas.lenght){
+    while(j < meninas.length){
         
-        console.log(`Casal: ${meninos[i]} e ${meninas(i)}`); //[posição dentro lista]
+        console.log(`Casal: ${meninos[i]} e ${meninas[j]}`); //[posição dentro lista]
         j++;
     }
     
